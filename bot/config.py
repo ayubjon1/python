54a4bdc1c526184707636c39278da6907d0007ef
ayubjon1 <116214067+ayubjon1@users.noreply.github.com>
2022-10-22 02:24:08 +0700

    Add files via upload

---
+++ bot/config.py
@@ -0,0 +1,10 @@
+
+#BOT:
+TOKEN = "5391994620:AAGg7nFAOdUn_Rp9q85UODC50Ir5riFT2DU" # ТОКЕН БОТА
+ADMIN = 5651277183 # ИД АДМИНА, узнать свой айди - @get_any_telegram_id_bot
+
+DIR = "" # Путь к файлу, если нужно
+
+#USER: # https://my.telegram.org/auth
+API_ID = 21357475
+API_HASH = "ce04257a6a5acaadbb4897a132da4b95"
